483415c21e7077dc77777705a45679436a69041c
Kento Kobayashi <kobayashi_kento@nomadinc.co.jp>
2015-06-08 20:36:17 +0900

    refactoring

--- app/src/main/java/com/pliseproject/views/adapters/SimpleDrawerAdapter.java
+++ app/src/main/java/com/pliseproject/views/adapters/SimpleDrawerAdapter.java
@@ -19,7 +19,6 @@ import java.util.Map;
 
 import static butterknife.ButterKnife.findById;
 
-
 /**
  * NavigationDrawerのAdapterクラス。
  */
@@ -48,14 +47,11 @@ public class SimpleDrawerAdapter extends RecyclerView.Adapter<SimpleDrawerAdapte
             // viewTypeによって値を設定
             switch (viewType) {
                 case R.array.type_header_before_login:
-//                    mTextView = findById(itemView, R.id.email);
                     mImageView = findById(itemView, R.id.icon);
                     findById(itemView, R.id.drawer_header_layout)
                             .setBackgroundColor(context.getResources().getColor(R.color.action_bar));
                     break;
                 case R.array.type_header_after_login:
-//                    mTextView = findById(itemView, R.id.name);
-//                    mTextView2 = findById(itemView, R.id.email);
                     mImageView = findById(itemView, R.id.icon);
                     findById(itemView, R.id.drawer_header_layout)
                             .setBackgroundColor(context.getResources().getColor(R.color.action_bar));
