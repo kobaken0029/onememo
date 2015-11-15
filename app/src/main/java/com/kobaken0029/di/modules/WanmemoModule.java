94c77a7ddd204417df8ecba1dde2e2652d2e7623
kento_kobayashi <kobaken0029@gmail.com>
2015-11-16 02:20:24 +0900

    fix di module

--- app/src/main/java/com/kobaken0029/di/modules/WanmemoModule.java
+++ app/src/main/java/com/kobaken0029/di/modules/WanmemoModule.java
@@ -1,7 +1,9 @@
 package com.kobaken0029.di.modules;
 
+import com.kobaken0029.helpers.ToolbarHelper;
 import com.kobaken0029.helpers.impls.MemoHelperImpl;
 import com.kobaken0029.helpers.MemoHelper;
+import com.kobaken0029.helpers.impls.ToolbarHelperImpl;
 
 import javax.inject.Singleton;
 
@@ -10,6 +12,13 @@ import dagger.Provides;
 
 @Module
 public class WanmemoModule {
+
+    @Provides
+    @Singleton
+    ToolbarHelper provideToolbarHelper() {
+        return new ToolbarHelperImpl();
+    }
+
     @Provides
     @Singleton
     MemoHelper provideMemoHelper() {
